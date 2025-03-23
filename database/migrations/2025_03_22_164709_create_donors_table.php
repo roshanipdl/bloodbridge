1761b32d080fb294f55037283efe3be486d25bb1
Roshani Paudel <roshani@Roshanis-MacBook-Air.local>
2025-03-23 22:02:31 +0545

    project setup

---
+++ database/migrations/2025_03_22_164709_create_donors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('donors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('blood_group');
+            $table->string('contact');
+            $table->string('address');
+            $table->date('last_donation_date');
+            $table->foreignID('user_id')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('donors');
+    }
+};
---
+++ database/migrations/2025_03_22_165248_create_blood_requests_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blood_requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('status');
+            $table->date('fulfill_date');
+            $table->date('request_date');
+            $table->foreignId('donor_id')->onDelete('cascade')->nullabe();
+            $table->foreignId('recipient_id')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blood_requests');
+    }
+};
